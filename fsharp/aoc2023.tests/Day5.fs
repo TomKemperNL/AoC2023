4064b8d25db6f77437c5acedfa17c735a5db2380
Tom <tomkempernl@gmail.com>
2023-12-05 18:38:37 +0100

    Parsing seems to work?

---
+++ fsharp/aoc2023.tests/Day5.fs
@@ -0,0 +1,46 @@
+﻿module aoc2023.Day5.tests
+
+open System.IO
+open NUnit.Framework
+
+let example = """seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4"""      .Split("\n") |> Array.toList
+
+[<Test>]
+let day5AExample () =
+    namesOfKinds
+    Assert.AreEqual(35, day5a (parse example) )
+
+let input =
+    File.ReadLines "./Day5.txt" |> Array.ofSeq
---
+++ fsharp/aoc2023/Day5.fs
@@ -0,0 +1,98 @@
+﻿module aoc2023.Day5
+
+open Microsoft.FSharp.Reflection
+open Shared
+
+type StartingSeed = StartingSeed of int
+
+type ItemKind = //Not necessary, but lets pretend the domain matters...
+    | Seed
+    | Soil
+    | Fertilizer
+    | Water
+    | Light
+    | Temperature
+    | Humidity
+    | Location
+
+let namesOfKinds =
+    let cases =
+        FSharpType.GetUnionCases(typeof<ItemKind>)
+
+    let values =
+        Array.map (fun c -> FSharpValue.MakeUnion(c, [||]) :?> ItemKind) cases
+
+    Array.zip (Array.map (fun (s: UnionCaseInfo) -> s.Name.ToLower()) cases) values
+    |> Map.ofArray
+
+type Item = (int * ItemKind)
+
+
+type AlmanacMapping =
+    { DestinationRangeStart: int
+      SourceRangeStart: int
+      RangeLength: int
+      From: ItemKind
+      To: ItemKind }
+
+
+type Input =
+    { Seeds: StartingSeed list
+      Mapping: AlmanacMapping list }
+
+type ParseResult =
+    { Seeds: int list
+      CurrentFrom: ItemKind option
+      CurrentTo: ItemKind option
+      Mappings: AlmanacMapping list }
+
+let parse lines : Input =
+    let rec parseR (sofar: ParseResult) (lines: string list) : Input =
+        match lines with
+        | [] ->
+            { Seeds = List.map StartingSeed sofar.Seeds
+              Mapping = sofar.Mappings }
+        | (h :: t) ->
+            match h with
+            | _ when System.String.IsNullOrEmpty(h.Trim()) -> parseR sofar t
+            | ParseRegex "seeds: (.+)" [ seednrsString ] ->
+                let seedNrs =
+                    seednrsString.Split(" ")
+                    |> Array.map int
+                    |> List.ofArray
+
+                let newSofar =
+                    { sofar with Seeds = seedNrs }
+
+                parseR newSofar t
+            | ParseRegex "(.+)-to-(.+) map:" [ from; target ] ->
+                let fromKind = Map.find from namesOfKinds
+                let toKind = Map.find target namesOfKinds
+
+                let newSofar =
+                    { sofar with
+                        CurrentFrom = Some fromKind
+                        CurrentTo = Some toKind }
+
+                parseR newSofar t
+            | ParseRegex "(\\d+) (\\d+) (\\d+)" [ destStart; sourceStart; length ] ->
+                let newMapping =
+                    { DestinationRangeStart = (int destStart)
+                      SourceRangeStart = (int sourceStart)
+                      RangeLength = (int length)
+                      From = sofar.CurrentFrom |> Option.get
+                      To = sofar.CurrentTo |> Option.get }
+
+                parseR { sofar with Mappings = (newMapping :: sofar.Mappings) } t
+            | _ -> failwith ("todo: " + h)
+
+    parseR
+        { Seeds = []
+          Mappings = []
+          CurrentFrom = None
+          CurrentTo = None }
+        lines
+
+
+let day5a almanac =
+    42
