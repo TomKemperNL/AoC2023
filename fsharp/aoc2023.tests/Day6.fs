a395733ee4283c42c47b863b066abbdcb3942ba0
Tom <tomkempernl@gmail.com>
2023-12-06 15:11:33 +0100

    Todo: wiskunde. Dit is genant

---
+++ fsharp/aoc2023.tests/Day6.fs
@@ -0,0 +1,45 @@
+﻿module aoc2023.Day6.tests
+
+open System.IO
+open NUnit.Framework
+
+let example = [
+    { Time = 7; Distance= 9 }
+    { Time = 15; Distance= 40 }
+    { Time = 30; Distance= 200 }
+]   
+    
+let exampleB = [
+    { Time = 71530; Distance= 940200 }
+]
+
+let input = [
+    { Time = 53; Distance= 333 }
+    { Time = 83; Distance= 1635 }
+    { Time = 72; Distance= 1289 }
+    { Time = 88; Distance= 1532 }
+]
+
+let inputB = [
+    { Time = 53837288; Distance= 333163512891532L }
+]
+
+[<Test>]
+let day6aExampleTest () =
+    Assert.AreEqual(288, day6a example)
+
+[<Test>]    
+let day6aTest () =
+    Assert.AreEqual(140220, day6a input)
+    ()
+
+[<Test>]
+let day6bExampleTest () =
+    Assert.AreEqual(71503, (day6a exampleB))
+    ()
+
+[<Test>]    
+let day6bTest () =
+    Assert.AreEqual(39570185L, day6a inputB)
+    ()
+    
---
+++ fsharp/aoc2023/Day6.fs
@@ -0,0 +1,28 @@
+﻿module aoc2023.Day6
+
+open System.Collections.Generic
+
+type Race = {
+    Time : int64
+    Distance: int64 
+}
+
+let determineDistance maxTime timeHeld =
+    timeHeld * (maxTime - timeHeld)
+
+let raceOptions maxTime =
+    let options = [0L .. maxTime]
+    List.map (determineDistance maxTime) options
+
+let winningOptions race =
+    List.filter (fun n -> n > race.Distance) (raceOptions race.Time)
+
+let day6a (input: Race list) =
+    let nrOfOptions race =
+        List.length (winningOptions race)
+    
+    let optionsPerRace = List.map nrOfOptions input
+    List.fold (*) 1 optionsPerRace
+
+let day6b input =
+    42    
