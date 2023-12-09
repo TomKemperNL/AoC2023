4a294fdf3ca4165f3ea905f20ea6971f8fdcede4
Tom <tomkempernl@gmail.com>
2023-12-09 12:40:33 +0100

    Als A zo mee valt maak ik me zorgen om B...

---
+++ fsharp/aoc2023.tests/Day9.fs
@@ -0,0 +1,18 @@
+﻿module aoc2023.Day9.tests
+
+open System.IO
+open NUnit.Framework
+
+let example = """0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45""".Split("\n") |> List.ofArray
+
+let input = File.ReadLines "./Day9.txt" |> List.ofSeq
+
+[<Test>]
+let day9aExampleTest () =
+    Assert.AreEqual(114, day9a (parse example))
+
+[<Test>]
+let day9aTest () =
+    Assert.AreEqual(1772145754L, day9a (parse input))    
---
+++ fsharp/aoc2023/Day9.fs
@@ -0,0 +1,37 @@
+﻿module aoc2023.Day9
+
+open Microsoft.FSharp.Core
+
+type History = int64 list
+
+let rec parseLine (line: string) : History =
+    line.Trim().Split(" ") |> Array.map int64 |> List.ofArray
+
+let parse lines : History list =
+    List.map parseLine lines
+
+let predictNext (history: History) : int64 =
+    
+    let differences hist =        
+        let pairs = List.pairwise hist
+        List.map (fun (a,b) -> b - a) pairs
+        
+    let rec collectDifferences hist : int64 list list=
+        let diffs = differences hist
+        if List.forall ((=) 0L) diffs then
+            [diffs]
+        else
+            diffs :: collectDifferences diffs 
+    
+    let difList : int64 list list = collectDifferences history
+    
+    let rec predictNext (diffs: int64 list list) =
+        match diffs with
+        | [] -> 0L
+        | d :: rest ->
+            List.last d + (predictNext rest)
+    
+    predictNext (history :: difList)
+
+let day9a input =
+    List.map predictNext input |> List.sum
