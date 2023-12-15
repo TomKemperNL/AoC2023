8d7df5356ae0d09a53dc9054980af7ae2f7ef39c
Tom <tom.kemper@hu.nl>
2023-12-15 09:23:14 +0100

    14A is best straightforward

---
+++ fsharp/aoc2023.tests/Day14.fs
@@ -0,0 +1,38 @@
+﻿module aoc2023.Day14.tests
+
+open System.IO
+open NUnit.Framework
+
+let example = """O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#...."""   .Split("\n")
+
+[<Test>]
+let dropTests () =
+    let result = drop [Empty;Round;Empty;Round]
+    Assert.AreEqual([Round; Round; Empty; Empty], result)
+    
+    let result = drop [Empty;Round;Square;Empty;Round]
+    Assert.AreEqual([Round; Empty; Square; Round; Empty], result)
+    
+[<Test>]
+let weightTests () =
+    Assert.AreEqual(7, totalWeight [Round; Round; Empty; Empty])
+
+let input = File.ReadLines "./Day14.txt" |> Array.ofSeq
+
+[<Test>]
+let day14aExampleTest () =
+    Assert.AreEqual(136, day14a (parse example))
+
+[<Test>]
+let day14aTest () =
+    Assert.AreEqual(109833, day14a (parse input))
+    
---
+++ fsharp/aoc2023/Day14.fs
@@ -0,0 +1,52 @@
+﻿module aoc2023.Day14
+
+type Rock =
+    | Round
+    | Square
+    | Empty 
+
+let parse lines =
+     let rec parseLine y (line: string) : Rock array =
+        let chars = line.Trim().ToCharArray()         
+        let parseChar x c : Rock =
+            match c with
+            | 'O' -> Round
+            | '#' -> Square
+            | '.' -> Empty                
+        Array.mapi parseChar chars
+     Array.mapi parseLine lines
+
+let drop column =    
+    let rec dropRec col spaceToDrop =
+        match col with 
+        | [] -> spaceToDrop
+        | Empty :: rest -> dropRec rest (Empty::spaceToDrop)
+        | Round :: rest ->            
+            Round :: (dropRec rest spaceToDrop)
+        | Square :: rest ->
+            List.concat [spaceToDrop;[Square];dropRec rest []] 
+            
+    dropRec column []
+
+let column (panel: Rock[][]) n =
+    seq {
+        for y in 0..panel.Length - 1 do
+            yield panel[y][n]                
+    } |> Seq.toList
+    
+let totalWeight column =
+    let lngth = List.length column
+    let weight ix rock =
+        match rock with
+        | Round -> lngth - ix
+        | _ -> 0
+    Seq.mapi weight column |> Seq.sum
+    
+    
+let day14a (panel: Rock[][]) : int =
+    let width = panel[0].Length
+    [0..width-1]
+        |> List.map (column panel)
+        |> List.map drop 
+        |> List.map totalWeight
+        |> List.sum
